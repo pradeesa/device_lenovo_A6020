084b6df1042192ef08d88b3d41dc149111e2f277
karthick111 <karthickrko61@gmail.com>
2017-04-23 16:28:25 +0200

    A6020: Add TWRP Configurations
    Signed-off-by: karthick111 <karthickrko61@gmail.com>

--- product/ramdisk.mk
+++ product/ramdisk.mk
@@ -11,3 +11,9 @@ PRODUCT_PACKAGES += \
     init.qcom.ril.sh \
     init.qti.ims.sh \
     ueventd.qcom.rc
+
+# TWRP
+ifeq ($(WITH_TWRP),true)
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/twrp.fstab:recovery/root/etc/twrp.fstab
+endif
