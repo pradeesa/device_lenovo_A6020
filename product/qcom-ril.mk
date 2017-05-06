5aeabf5b4b6451ea6eff70498962150774b21f6b
karthick111 <karthickrko61@gmail.com>
2017-05-06 22:17:48 +0600

    A6020: Switch to crackling QMI, radio blobs

--- product/qcom-ril.mk
+++ product/qcom-ril.mk
@@ -1,8 +1,13 @@
+# QMI
+PRODUCT_PACKAGES += \
+    dsi_config.xml \
+    netmgr_config.xml \
+    qmi_config.xml
+
 # RIL
 PRODUCT_PACKAGES += \
     libcnefeatureconfig \
     librmnetctl \
     libshim_ril \
     libxml2 \
-    libril \
-    ril-wrapper
+    libril
