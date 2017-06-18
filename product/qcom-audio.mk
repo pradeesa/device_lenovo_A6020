6601de17bc02ac704d174b6ad6219e6f4707ab18
karthick111 <karthickrko61@gmail.com>
2017-06-18 15:47:38 +0200

    A6020: Import configs from vendor repo

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -4,6 +4,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths_qrd_skuh.xml \
+    $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml
+
 
 # Audio
 PRODUCT_PACKAGES += \
---
+++ product/sensors.mk
@@ -0,0 +1,3 @@
+# Sensor
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/calmodule.cfg:system/vendor/etc/calmodule.cfg
